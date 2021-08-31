c485f11dd4709d028867d04a08788d0526df1bb4
sayalihandore111 <sayalihandore111@gmail.com>
2021-08-31 20:37:36 +0530

    Add project files.

---
+++ TypeCasting/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TypeCasting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum;
+            Console.WriteLine("Enter two numbers");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            sum = num1 + num2;
+            Console.WriteLine("Result is " + sum);
+            Console.ReadLine();
+
+        }
+    }
+}
